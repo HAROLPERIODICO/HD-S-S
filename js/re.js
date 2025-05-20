f653abd34153e3fe1e55ac09706711fc95e3cf57
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-05-20 17:43:43 -0500

    Update re.js

--- js/re.js
+++ js/re.js
@@ -76,4 +76,14 @@ function aplicarFiltrosRE() {
   });
 }
 
+document.getElementById('mostrarTablaRE').addEventListener('click', function () {
+  const tabla = document.getElementById('contenedorTablaRE');
+  if (tabla.style.display === 'none') {
+    tabla.style.display = 'block';
+    this.textContent = 'Ocultar tabla RE';
+  } else {
+    tabla.style.display = 'none';
+    this.textContent = 'Mostrar tabla RE';
+  }
+});
 
