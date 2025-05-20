5c9a23e4a7ff6b8a81bd743ceb46fe7b9ffc019a
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-05-20 15:32:36 -0500

    Update re.js

--- js/re.js
+++ js/re.js
@@ -59,7 +59,7 @@ function aplicarFiltrosRE() {
   const prioridad = document.getElementById('filtroPrioridad').value.toLowerCase();
   const turno = document.getElementById('filtroTurno').value.toLowerCase();
   const accion = document.getElementById('filtroAccion').value.toLowerCase();
-  
+  const fecha = document.getElementById('filtroFecha').value;
 
   document.querySelectorAll('#reTable tbody tr').forEach(row => {
     const celdas = row.querySelectorAll('td');
@@ -68,7 +68,8 @@ function aplicarFiltrosRE() {
       (prioridad === '' || celdas[4].textContent.toLowerCase() === prioridad) &&
       (grupo === '' || celdas[7].textContent.toLowerCase() === grupo) &&
       (turno === '' || celdas[6].textContent.toLowerCase() === turno) &&
-      (accion === '' || celdas[8].textContent.toLowerCase() === accion)
+      (accion === '' || celdas[8].textContent.toLowerCase() === accion) &&
+    (fecha === '' || celdas[0].textContent === fecha);
      
 
     row.style.display = match ? '' : 'none';
