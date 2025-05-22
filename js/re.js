5fb84dd82c2a023a37bd41dbb5e3349fd9e1d2fd
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-05-22 13:46:26 -0500

    Update re.js

--- js/re.js
+++ js/re.js
@@ -83,37 +83,3 @@ function convertirArchivoABase64(file) {
 document.addEventListener('DOMContentLoaded', cargarDatosRE);
 
 
-
-document.getElementById('mostrarTablaRE').addEventListener('click', function () {
-  const tabla = document.getElementById('contenedorTablaRE');
-  if (tabla.style.display === 'none') {
-    tabla.style.display = 'block';
-    this.textContent = 'Ocultar tabla RE';
-  } else {
-    tabla.style.display = 'none';
-    this.textContent = 'Mostrar tabla RE';
-  }
-});
-
-
-const btnMostrar = document.getElementById('mostrarTablaRE');
-const contenedorTabla = document.getElementById('contenedorTablaRE');
-
-btnMostrar.addEventListener('click', function () {
-  if (contenedorTabla.classList.contains('mostrar')) {
-    contenedorTabla.classList.remove('mostrar');
-    // Esperar la transición antes de ocultar completamente
-    setTimeout(() => {
-      contenedorTabla.style.display = 'none';
-    }, 300);
-    this.textContent = 'Mostrar tabla RE';
-  } else {
-    contenedorTabla.style.display = 'block';
-    setTimeout(() => {
-      contenedorTabla.classList.add('mostrar');
-    }, 10);
-    this.textContent = 'Ocultar tabla RE';
-  }
-});
-
-
